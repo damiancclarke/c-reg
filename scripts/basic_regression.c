83baece78675b7268c00420163c10609f9eb580d
Damian Clarke <damian.c.clarke@gmail.com>
2012-08-25 21:55:36 -0400

    Early version of regression_input.c

--- scripts/basic_regression.c
+++ scripts/basic_regression.c
@@ -9,28 +9,35 @@ int main(int argc, char *argv[])
 	}*/
 
 	int y[] = {23, 21, 18, 15, 6, 12, 7, 3, 0};
-	int x[] = {430, 350, 330, 220, 120, 100, 90, 80, 10};
+	int x[][] = {
+					430, 350, 330, 220, 120, 100, 90, 80, 10,
+				 	1, 0, 1, 1, 0, 1, 0, 0, 0
+				};
 	double xx = 0; 
 	double xy = 0;
 	double sum = 0;
 	double beta = 0;
+	char *vars[] ={"x1", "x2"};
 
 	//get the size of the x and y vector
 	int count_y = sizeof(y) / sizeof(int);
-	int count_x = sizeof(x) / sizeof(int);
+	int count_x = sizeof(x[1]) / sizeof(int);
+	int count_vars = sizeof(vars) / sizeof(int);
 
 	//create the vectors x'x and x'y
 	int i = 0;
-	for(i = 0; i < count_x; i++) {
-		sum = sum + x[i]*x[i];
-	}
-	xx = sum;	
-	printf("The product of this vector is %f:\n ", xx);
+//	int j = 0;
+//	for(j = 0; j < count_vars; j++) {
+		for(i = 0; i < count_x; i++) {
+			sum = sum + x[1][i]*x[1][i];
+		}
+		xx = sum;	
+		printf("The product of this vector is %f:\n ", xx);
 
 	sum = 0;
 
 	for(i = 0; i < count_y; i++) {
-		sum = sum + x[i]*y[i];
+		sum = sum + x[1][i]*y[i];
 	}
 	xy = sum;	
 	printf("The product of this vector is %f:\n ", xy);
@@ -40,5 +47,6 @@ int main(int argc, char *argv[])
 	beta = beta*xy;
 	printf("beta is %e:\n ", beta);
 
+	printf("counts: %d, %d, %d\n", count_y, count_x, count_vars);
 	return 0;
 }
